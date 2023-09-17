dbac2bd5e485ab19277fbb962e326307c047ffed
Priyalakshmi7 <priya.lakshmi@thoughtworks.com>
2023-09-17 21:05:00 +0530

    Add welcome image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Navbar from './components/navBar'
+import Welcome from './components/welcome'
 
 function App() {
 
   return (
     <div className="App">
       <Navbar/>
+      <Welcome/>
     </div>
   )
 }
