b9fdd3834c0171e9a8c3bdad1e0f8a06bae3e7a7
Priyalakshmi7 <priya.lakshmi@thoughtworks.com>
2023-09-17 21:53:52 +0530

    Add cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Card from './components/card'
 import Navbar from './components/navBar'
 import Welcome from './components/welcome'
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Welcome/>
+      <Card/>
     </div>
   )
 }
