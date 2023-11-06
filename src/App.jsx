dabf88ade8ad5badc6aa3df6d9330e7952a7ad0d
Priyalakshmi7 <priya.lakshmi@thoughtworks.com>
2023-11-07 00:46:50 +0530

    Api call to add items to cart

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import AddToCart from './pages/AddToCart';
+import CartItems from './pages/CartItems';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/addtocart" element={<AddToCart />} />
+        <Route path="/cart" element={<CartItems />} />
       </Routes>
     </Router>
   );
